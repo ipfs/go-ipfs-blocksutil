0e31e9045b7f3cc4226688ec5dbdb519e1e39f61
Hector Sanjuan <code@hector.link>
2018-04-03 14:53:30 +0200

    Extract: gx, readme, license, makefile, travis

--- block_generator.go
+++ block_generator.go
@@ -2,7 +2,7 @@
 // with Blocks.
 package blocksutil
 
-import "gx/ipfs/Qmej7nf81hi2x2tvjRBF3mcp74sQyuDH4VMYDGd1YtXjb2/go-block-format"
+import "github.com/ipfs/go-block-format"
 
 // NewBlockGenerator returns an object capable of
 // producing blocks.
